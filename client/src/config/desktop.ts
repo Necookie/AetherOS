e85d58e0919db9543deab93dce2772cb50ad0ab3
Necookie <dheyn.main@gmail.com>
2026-03-13 11:10:20 +0800

    feat: desktop icons now working

--- client/src/config/desktop.ts
+++ client/src/config/desktop.ts
@@ -3,20 +3,16 @@ export const DESKTOP_WALLPAPER_URL = '/assets/wallpapers/urban-night-street.jpg'
 export const DESKTOP_ICONS = [
     {
         id: 'pc',
+        appId: 'explorer',
         label: 'This PC',
         iconUrl: 'https://img.icons8.com/color/48/000000/monitor--v1.png',
         alt: 'This PC'
     },
     {
         id: 'settings',
+        appId: 'settings',
         label: 'Settings',
         iconUrl: 'https://img.icons8.com/color/48/000000/settings--v1.png',
         alt: 'Settings'
-    },
-    {
-        id: 'appstore',
-        label: 'App Store',
-        iconUrl: 'https://img.icons8.com/color/48/000000/shopping-basket-2--v1.png',
-        alt: 'App Store',
     },
 ]
